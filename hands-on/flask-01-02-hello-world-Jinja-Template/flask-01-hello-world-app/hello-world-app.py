20ed7e9db3e80e59f4e697641bfc1ee300f028b5
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-04-27 23:05:35 -0400

    Flask_01_April27

---
+++ hands-on/flask-01-02-hello-world-Jinja-Template/flask-01-hello-world-app/hello-world-app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def head():
+    return '<h1>Hello World!</h1>'
+
+@app.route("/second")
+def second():
+    return 'This is my second page'
+
+@app.route("/third/subthird")
+def third():
+    return "<h2>This is my third page!</h2>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
