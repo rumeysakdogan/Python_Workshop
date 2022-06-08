41170bae206c6ba98e322bf388290851af93d65b
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-06-07 21:44:37 -0400

    updating app.config

--- hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
+++ hands-on/flask-05-Handling-SQL-with-Flask-Web-Application/app-with-mysql.py
@@ -6,7 +6,7 @@ from flaskext.mysql import MySQL
 app = Flask(__name__)
 
 # Configure mysql database
-app.config['MYSQL_DATABASE_HOST'] = 'PLEASE WRITE YOUR DATABASE ENDPOINT HERE'
+app.config['MYSQL_DATABASE_HOST'] = 'msqual-portfolio-db.chrgxmhxkprk.us-east-1.rds.amazonaws.com'
 app.config['MYSQL_DATABASE_USER'] = 'admin'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Clarusway_1'
 app.config['MYSQL_DATABASE_DB'] = 'clarusway'
