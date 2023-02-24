2b70fdce79b103c810134ef68a85f68c8d4012b2
rumeysakdogan <dgn.rumeysaa@gmail.com>
2023-02-23 20:19:57 -0500

    add robot-barista

---
+++ coding-challenges/math-with-python/main.py
@@ -0,0 +1,16 @@
+name = "Rumeysa"
+
+age = 36  # integer
+
+print(f"Happy {age}th Birthday {name}!!🎂")
+
+actual_age = 36.12  # float
+
+math = 5 ** 7 + 6 / 9 * 5 - 4
+
+results = age + actual_age + math
+
+print(results)
+
+
+
---
+++ coding-challenges/robot-barista/main.py
@@ -0,0 +1,21 @@
+print("Welcome to Cloud Coffee!!")
+
+customer_name = input("What is your name?: ")
+
+menu = "Black Coffee, Espresso, Latte, Capuccino"
+
+print(f"Hello {customer_name}, What would you like from our menu today ? Here is what we are serving: \n {menu}")
+
+order = input("Please enter your order: ")
+
+price = 8
+
+quantity = int(input("How many coffees would you want?: "))
+
+total = price * quantity
+
+print(f"Sounds good, we'll have that {order} ready for you in a moment!")
+
+print(f"Thank you! Your total is {total}")
+
+
